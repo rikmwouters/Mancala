f234c2006177483c1dbefd742e05af44521eefb8
Rik Wouters <rikwouters@sogyo.nl>
2018-07-03 10:48:23 +0200

    Added test that deliberately fails

---
+++ src/test/java/nl/sogyo/mancala/domain/TestTest.java
@@ -0,0 +1,17 @@
+package nl.sogyo.mancala.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		Boolean test = false;
+		Assert.assertTrue(test);
+	}
+
+}
